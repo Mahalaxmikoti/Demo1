4cb1a335156ee3a4c9ea8dddbac1e86acbb18fde
maata <maatakoti12345@gmail.com>
2024-08-16 22:29:31 +0530

    T2 tester work 2 done

--- src/test/java/Steps/Demo1Step.java
+++ src/test/java/Steps/Demo1Step.java
@@ -2,7 +2,9 @@ package Steps;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -51,6 +53,14 @@ public class Demo1Step {
 	public void user_selects_radio_button() {
 	    
 		driver.findElement(By.xpath("//input[@value='1']")).click();
+	}
+	
+	@Then("user select birth year")
+	public void user_select_birth_year() {
+	    
+		WebElement wb= driver.findElement(By.name("birthday_year"));
+		Select sel= new Select(wb);
+		sel.selectByVisibleText("2000");
 	}
 
 }
